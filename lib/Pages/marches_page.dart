34f30d13fbb61eb8049f0d42a08e6d75154ae0c2
mathieulecl <ml78380@icloud.com>
2024-03-08 17:25:02 +0100

    initialisation projet

---
+++ lib/Pages/marches_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MarchesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text("Contenu de la page des Marchés"),
+      ),
+    );
+  }
+}
